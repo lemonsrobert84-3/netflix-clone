9f31a138c1069c3ea0d89cbf12c84e25292b86b3
lemonsrobert84-3 <lemonsrobert84.3@gmail.com>
2025-06-21 20:22:18 +0530

    Add files via upload

---
+++ netflix-clone-with-redirects_2/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="bg-black min-h-screen text-white flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600 mb-4">🍿 MyFlix</h1>
+      <p className="text-xl text-gray-400">Netflix Clone Home Page Coming Soon...</p>
+    </div>
+  );
+}
